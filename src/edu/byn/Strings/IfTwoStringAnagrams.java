7053aa6a468c182ec87abf1eeffe339bc03a4d8c
ybolotnyy@google.com <ybolotnyy@google.com>
2017-02-14 10:00:33 -0800

    anagram

---
+++ src/edu/byn/Strings/IfTwoStringAnagrams.java
@@ -0,0 +1,38 @@
+package edu.byn.Strings;
+
+/**
+ * Created by ybolotnyy on 2/14/17.
+ */
+public class IfTwoStringAnagrams {
+  /*a word, phrase, or name formed by rearranging the letters of another,
+   such as cinema, formed from iceman; listen vs silent; solve vs loves*/
+
+  public static void main(String[] args) {
+    System.out.println(ifTwoStringsAnagram("listen", "silent"));
+  }
+
+  private static boolean ifTwoStringsAnagram(String s, String s1) {
+    System.out.printf("checking '%s' is anagram of '%s' - ", s, s1);
+
+    int[] letters = new int[256];
+    int uniqueLetters = 0;
+
+    for (int i = 0; i < s.length(); i++) {
+      int value = s.charAt(i);
+      if (letters[value] == 0) {
+        ++uniqueLetters;
+      }
+      ++letters[value];
+    }
+
+    for (int i = 0; i < s1.length(); i++) {
+      int value = s1.charAt(i);
+      --letters[value];
+      if (letters[value] < 0) { // this letter not found in s
+        return false;
+      } else --uniqueLetters;
+    }
+
+    return uniqueLetters == 0;
+  }
+}
