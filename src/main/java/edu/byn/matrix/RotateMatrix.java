5411284ef929ab08e081fa9c5c73bf39eac87d80
ybolotnyy@google.com <ybolotnyy@google.com>
2017-02-17 08:48:40 -0800

    refactored

--- src/main/java/edu/byn/matrix/RotateMatrix.java
+++ src/main/java/edu/byn/matrix/RotateMatrix.java
@@ -7,28 +7,9 @@ import static main.java.edu.byn.utils.Utils.*;
  */
 public class RotateMatrix {
   public static void main(String[] args) {
-    int[][] matrix = {
-            {1,2,3,4,5},
-            {1,2,3,4,5},
-            {1,2,3,4,5},
-            {1,2,3,4,5},
-            {1,2,3,4,5}
-    };
-
-    print2dMatrix(matrix);
-    rotateSquareMatrix(matrix);
-    print2dMatrix(matrix);
-
-    int[][] matrix1 = {
-            {1,2,3,4},
-            {1,2,3,4},
-            {1,2,3,4},
-            {1,2,3,4}
-    };
-
-    print2dMatrix(matrix1);
-    rotateSquareMatrix(matrix1);
-    print2dMatrix(matrix1);
+    print2dMatrix(rotateSquareMatrix(getMatrix(2)));
+    print2dMatrix(rotateSquareMatrix(getMatrix(5)));
+    print2dMatrix(rotateSquareMatrix(getMatrix(10)));
   }
 
   public static int[][] rotateSquareMatrix(int[][] m) {
@@ -37,11 +18,11 @@ public class RotateMatrix {
     for (int layer = 0; layer < (size / 2); layer++) {
       // rotate by layer starting from corners going clockwise
       for (int offset = 0; offset < size - 1 - layer * 2; offset++) {
-        int t = m[layer + 0][layer + 0 + offset];
-        m[layer + 0][layer + 0 + offset] = m[size - 1 - layer - offset][layer + 0];
-        m[size - 1 - layer - offset][layer + 0] = m[size - 1 - layer][size - 1 - layer - offset];
-        m[size - 1 - layer][size - 1 - layer - offset] = m[layer + 0 + offset][size - 1 - layer];
-        m[layer + 0 + offset][size - 1 - layer] = t;
+        int t = m[layer][layer + offset];
+        m[layer][layer + offset] = m[size - 1 - layer - offset][layer];
+        m[size - 1 - layer - offset][layer] = m[size - 1 - layer][size - 1 - layer - offset];
+        m[size - 1 - layer][size - 1 - layer - offset] = m[layer + offset][size - 1 - layer];
+        m[layer + offset][size - 1 - layer] = t;
 
 //        System.out.printf("\n layer=%d; offset=%d \n", layer, offset);
 //        print2dMatrix(m);
--- src/main/java/edu/byn/utils/Utils.java
+++ src/main/java/edu/byn/utils/Utils.java
@@ -4,6 +4,18 @@ package main.java.edu.byn.utils;
  * Created by ybolotnyy on 2/16/17.
  */
 public class Utils {
+  public static int[][] getMatrix(int size) {
+    int[][] m = new int[size][size];
+
+    for (int y = 0; y < size; y++) {
+      for (int x = 0; x < size; x++) {
+        m[y][x] = x;
+      }
+    }
+
+    return m;
+  }
+
   public static void print2dMatrix(int[][] m) {
     int rowsNum = m.length;
     int columnsNum = m[0].length;
