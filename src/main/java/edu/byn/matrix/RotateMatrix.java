15c93b871251ec60761e1495d9dd0c9c4813d063
byn <yuriy.bolotnyy@gmail.com>
2017-02-16 19:11:16 -0800

    first matrix layer rotates

--- src/main/java/edu/byn/matrix/RotateMatrix.java
+++ src/main/java/edu/byn/matrix/RotateMatrix.java
@@ -29,23 +29,13 @@ public class RotateMatrix {
     int size = m.length;
 
     // rotate by layer starting from corners going clockwise
-    for (int layer = 0; layer < size / 2; layer++) {
-      for (int offset = 0; offset < size / 2; offset++) {
-        int tmp = m[layer][offset];
-        m[layer][offset] = m[size-1-offset][offset];
-        m[size-1-offset][offset] = m[size-1-layer][size-1-offset];
-        m[size-1-layer][size-1-offset] = m[layer+offset][size-1-offset];
-        m[layer+offset][size-1-offset] = tmp;
-      }
+    for (int offset = 0; offset < size - 1; offset++) {
+      int t = m[0][0 + offset];
+      m[0][0 + offset] = m[size - 1 - offset][0];
+      m[size - 1 - offset][0] = m[size - 1][size - 1 - offset];
+      m[size - 1][size - 1 - offset] = m[0 + offset][size - 1];
+      m[0 + offset][size - 1] = t;
     }
-
-
-/*
-    int tmp = m[0][0]; // tmp = m[layer][offset]
-    m[0][0] = m[4][0]; // m[layer][offset] = m[size-1-offset][offset]
-    m[4][0] = m[4][4]; // m[size-1-offset][offset] = m[size-1-layer][size-1-offset]
-    m[4][4] = m[0][4]; // m[size-1-layer][size-1-offset] = m[layer+offset][size-1-offset]
-    m[0][4] = tmp;*/
 
     return m;
   }
