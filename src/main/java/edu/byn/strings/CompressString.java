685019f41ab4dbb2371c8c6c540ed1207de70553
ybolotnyy@google.com <ybolotnyy@google.com>
2017-02-21 14:12:09 -0800

    compress string implemented by char arrayList

--- src/main/java/edu/byn/strings/CompressString.java
+++ src/main/java/edu/byn/strings/CompressString.java
@@ -1,5 +1,7 @@
 package main.java.edu.byn.strings;
 
+import java.util.ArrayList;
+
 /**
  * Created by ybolotnyy on 2/17/17.
  */
@@ -11,14 +13,36 @@ public class CompressString {
   private static String compressString(char[] s) {
     int len = s.length;
 //     save first char to output array
-    char[] output =
+    ArrayList<Character> output = new ArrayList<>();
+    output.add(s[0]);
+    int dupesCount = 0;
 //     scan string char by char starting from 2nd char
+    for (int i = 1; i < len; i++) {
+      char currentChar = s[i];
+      char previousChar = s[i-1];
 //     if current char is dupe of previous,
+      if (currentChar == previousChar) {
 //       increment dupesCounter
+        dupesCount++;
+      } else {
+        output.add(currentChar);
+      }
+
+      if (currentChar != previousChar && dupesCount > 0){
 //     if current char become not dupe of previous,
 //       add number of dupes (as digit) to output
 //       reset dupesCounter
+        System.out.printf("% dupes of %s found \n", dupesCount, previousChar);
+        output.add((char) dupesCount);
+        dupesCount = 0;
+      }
 
-    return new String(s);
+      if ( (dupesCount > 0) && (i == len - 1) ) {
+        output.add((char) dupesCount);
+      }
+
+    }
+
+    return new String(output.toString());
   }
 }
