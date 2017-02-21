0c8933fb0c0bb78f2e6be274323f35c246269982
ybolotnyy@google.com <ybolotnyy@google.com>
2017-02-21 14:29:47 -0800

    refactored string compression using StringBuilder

--- src/main/java/edu/byn/strings/CompressString.java
+++ src/main/java/edu/byn/strings/CompressString.java
@@ -12,37 +12,35 @@ public class CompressString {
 
   private static String compressString(char[] s) {
     int len = s.length;
+    StringBuilder out = new StringBuilder();
 //     save first char to output array
     ArrayList<Character> output = new ArrayList<>();
-    output.add(s[0]);
+    out.append(s[0]);
     int dupesCount = 0;
-//     scan string char by char starting from 2nd char
+    // scan string char by char starting from 2nd char
     for (int i = 1; i < len; i++) {
       char currentChar = s[i];
       char previousChar = s[i-1];
-//     if current char is dupe of previous,
+      // if current char is dupe of previous,
       if (currentChar == previousChar) {
-//       increment dupesCounter
+        // increment dupesCounter
         dupesCount++;
-      } else {
-        output.add(currentChar);
-      }
-
-      if (currentChar != previousChar && dupesCount > 0){
-//     if current char become not dupe of previous,
-//       add number of dupes (as digit) to output
-//       reset dupesCounter
+      } else if (currentChar != previousChar && dupesCount > 0){
+        // if current char become not dupe of previous,
+        // add number of dupes (as digit) to output
+        // reset dupesCounter
         System.out.printf("% dupes of %s found \n", dupesCount, previousChar);
-        output.add((char) dupesCount);
+        out.append(dupesCount);
+        out.append(currentChar);
         dupesCount = 0;
-      }
+      } else
+        out.append(currentChar);
 
       if ( (dupesCount > 0) && (i == len - 1) ) {
-        output.add((char) dupesCount);
+        out.append(dupesCount);
       }
-
     }
 
-    return new String(output.toString());
+    return out.toString();
   }
 }
