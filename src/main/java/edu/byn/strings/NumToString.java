66816bf165a0fd1e7f1f15f827622b8ca385b56c
byn <yuriy.bolotnyy@gmail.com>
2017-02-24 21:28:14 -0800

    num to string started

--- src/main/java/edu/byn/strings/NumToString.java
+++ src/main/java/edu/byn/strings/NumToString.java
@@ -1,19 +1,20 @@
 package main.java.edu.byn.strings;
 
 import main.java.edu.byn.utils.Matrix;
+import main.java.edu.byn.utils.Print;
+
+import static main.java.edu.byn.utils.Print.prt;
 
 public class NumToString {
   public static void main(String[] args) {
-    System.out.println(numToString(321));
+    System.out.println(numToString(9325));
   }
 
   private static String numToString(int num) {
     StringBuffer sb = new StringBuffer();
 
-    int len = 1;
-    int i = 0;
-    while (Math.pow((double) 10, (double) ++i) < num) {
-      ++len;
+    int len = 0;
+    while (Math.pow((double) 10, (double) ++len) < num) {
     }
 
     String[] wordArray1 = {"One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"};
@@ -21,23 +22,26 @@ public class NumToString {
     String[] wordArray10 = {"Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
     String[] wordArray100 = {"Hundred", "Thousand"};
 
-    int reminderSum = 0;
-    for (i = 0; i < len; i++) {
-      int divider = (int) Math.pow((double) 10, (double) i + 1);
-      // i = 1    321 % 10 = 1
-      // i = 2    321 % 100 = 21     - 1 ) / 10
-      // i = 2    321 % (10^i) = 21
+      // i = 2    321 % 100 = 21 / 10 = int 2
+      //          (num % 100) / 10^1
+      //          (num % 10^i) / 10^(i-1)
+    int word11 = num % 100;
 
-      // i = 3    321 % 1000 = 321   - 21) / 100
-      // i = 3    321 % (10^i) - 21 / 100
-      // i = 3    ( 321 % (10^i) - 321 % (10^(i-1)) ) / 10^(i-1)
+    for (int i = len; i > 0; i--){
+      int digit = (int) ((num % Math.pow(10,i)) / Math.pow(10,i-1));
 
-      int digitToConvert = (int) ((
-                  num % Math.pow((double)10, (double) i)
-                  - num % Math.pow((double)10, (double) i - 1))
-                  / Math.pow((double)10, (double) i - 1));
-      sb.insert(0, wordArray1[(int) (digitToConvert % 10) ] + " ");
-
+      if (i > 2 || ( i == 1 && word11 > 20)) {
+        sb.append(wordArray1[digit - 1] + " ");
+      }
+      if (i > 3) {
+        sb.append(wordArray100[1] + " ");
+      } else if (i > 2) {
+        sb.append(wordArray100[0] + " ");
+      }  else if (i > 1 && digit > 1) {
+        sb.append(wordArray10[digit - 1] + " ");
+      } else if (i == 1 && word11 < 20) {
+        sb.append(wordArray11[digit - 1] + " ");
+      }
     }
 
     return sb.toString();
---
+++ src/main/java/edu/byn/utils/Print.java
@@ -0,0 +1,10 @@
+package main.java.edu.byn.utils;
+
+/**
+ * Created by ybolotnyy on 2/24/17.
+ */
+public class Print {
+  public static void prt(String s){
+    System.out.println(s);
+  }
+}
