c373cbfb2dc59ea7ad3c6a4f99ba147a03ee49d2
byn <yuriy.bolotnyy@gmail.com>
2017-02-27 20:45:06 -0800

    words occurences started

--- src/main/java/edu/byn/strings/WordOccurencesFrequency.java
+++ src/main/java/edu/byn/strings/WordOccurencesFrequency.java
@@ -3,6 +3,7 @@ package main.java.edu.byn.strings;
 import main.java.edu.byn.utils.TxtFileReader;
 
 import java.util.ArrayList;
+import java.util.Hashtable;
 
 import static main.java.edu.byn.utils.Print.prt;
 
@@ -13,16 +14,29 @@ public class WordOccurencesFrequency {
 
   public static void main(String[] args) {
     ArrayList<String> al = new TxtFileReader().getFileStrings("./src/main/resources/file/bible.txt");
-    System.out.println(wordOccurencesFrequency(al));
+    System.out.println(prepareDictionary(al));
   }
 
-  private static boolean wordOccurencesFrequency(ArrayList<String> textArray) {
-
+  private static boolean prepareDictionary(ArrayList<String> textArray) {
     if (textArray == null || textArray.size() == 0) return false;
 
+    Hashtable<String, Integer> ht = new Hashtable<>();
+
+    for (String string : textArray) {
+      for (String word : string.split("\\W+")) {
+        word = word.toLowerCase();
+        prt(word);
+        if (!ht.containsKey(word)) {
+          ht.put(word, 1);
+        } else {
+          int occur = ht.get(word);
+          prt("word '" + word +  "' occurs " + occur + " times");
+          ht.put(word, ht.get(word) + 1);
+        }
+      }
+    }
 
     return true;
   }
-
 
 }
